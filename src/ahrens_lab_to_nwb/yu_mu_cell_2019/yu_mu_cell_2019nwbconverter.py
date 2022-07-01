024d403f10b02cd14b8f83e46e24a639e48f20d2
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2022-07-01 12:53:32 -0400

    Update yu_mu_cell_2019nwbconverter.py

--- src/ahrens_lab_to_nwb/yu_mu_cell_2019/yu_mu_cell_2019nwbconverter.py
+++ src/ahrens_lab_to_nwb/yu_mu_cell_2019/yu_mu_cell_2019nwbconverter.py
@@ -1,20 +1,15 @@
 """Primary NWBConverter class for this dataset."""
 from nwb_conversion_tools import (
     NWBConverter,
-    SpikeGLXRecordingInterface,
-    SpikeGLXLFPInterface,
-    PhySortingInterface,
+    Suite2PSegmentationInterface,
 )
 
 from ahrens_lab_to_nwb.yu_mu_cell_2019 import YuMuCell2019BehaviorInterface
 
 
 class YuMuCell2019NWBConverter(NWBConverter):
-    """Primary conversion class for my extracellular electrophysiology dataset."""
+    """Primary conversion class for this dataset."""
 
     data_interface_classes = dict(
-        Recording=SpikeGLXRecordingInterface,
-        LFP=SpikeGLXLFPInterface,
-        Sorting=PhySortingInterface,
-        Behavior=YuMuCell2019BehaviorInterface,
+        Segmentation=Suite2PSegmentationInterface,
     )
